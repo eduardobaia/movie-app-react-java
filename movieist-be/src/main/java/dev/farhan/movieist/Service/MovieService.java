cfb5f41cb0dc2951daac15da9c680fbdfb4232aa
josebaia <jose.baia@syone.com>
2023-11-30 11:15:19 +0000

    add project be and fe

---
+++ movieist-be/src/main/java/dev/farhan/movieist/Service/MovieService.java
@@ -0,0 +1,23 @@
+package dev.farhan.movieist.Service;
+
+import dev.farhan.movieist.model.Movie;
+import dev.farhan.movieist.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
